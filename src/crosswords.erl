e0259ec5492cc79f23e5ebd4b1cb55b0cbe1c3d6
Graham Hay <grahamrhay@gmail.com>
2019-04-14 16:10:19 +0100

    Validate words on grid

--- src/crosswords.erl
+++ src/crosswords.erl
@@ -16,16 +16,28 @@ generate_crossword(GridSize) ->
     generate_crossword(filter_words(WordList, GridSize), GridSize, [], #{}, 0).
 
 generate_crossword(WordList, GridSize, ChosenWords, Crossword, Failures) ->
-    case Failures of
+    case length(ChosenWords) of
         5 ->
             {ChosenWords, Crossword};
         _ ->
-            case add_word(WordList, GridSize, ChosenWords, Crossword) of
-                {true, NewChosenWords, NewCrossword} ->
-                    ?debugFmt("~p", [NewChosenWords]),
-                    generate_crossword(WordList, GridSize, NewChosenWords, NewCrossword, 0);
-                false ->
-                    generate_crossword(WordList, GridSize, ChosenWords, Crossword, Failures + 1)
+            case Failures of
+                5 ->
+                    {ChosenWords, Crossword};
+                _ ->
+                    case add_word(WordList, GridSize, ChosenWords, Crossword) of
+                        {true, NewChosenWords, NewCrossword} ->
+                            ?debugFmt("~p", [NewChosenWords]),
+                            AllWordsValid = check_words(WordList, NewCrossword, GridSize),
+                            ?debugFmt("All words valid ~p", [AllWordsValid]),
+                            case AllWordsValid of
+                                true ->
+                                    generate_crossword(WordList, GridSize, NewChosenWords, NewCrossword, 0);
+                                false ->
+                                    generate_crossword(WordList, GridSize, ChosenWords, Crossword, Failures + 1)
+                            end;
+                        false ->
+                            generate_crossword(WordList, GridSize, ChosenWords, Crossword, Failures + 1)
+                    end
             end
     end.
 
@@ -46,6 +58,7 @@ add_new_word(PossibleWords, Pos, Direction, Crossword, ChosenWords) ->
                 {true, NewCrossword} ->
                     {true, [Word | ChosenWords], NewCrossword};
                 false ->
+                    ?debugFmt("Unable to fit: ~s", [ChosenWord]),
                     RemainingWords = lists:filter(fun(W) -> W =/= ChosenWord end, PossibleWords),
                     add_new_word(RemainingWords, Pos, Direction, Crossword, ChosenWords)
             end
@@ -83,3 +96,86 @@ max_word_length({X, Y}, Direction, GridSize) ->
         down ->
             GridSize - X
     end.
+
+check_words(WordList, Crossword, GridSize) ->
+    case check_words_across(WordList, Crossword, GridSize) of
+        true ->
+            check_words_down(WordList, Crossword, GridSize);
+        false ->
+            false
+    end.
+
+check_words_across(WordList, Crossword, GridSize) ->
+    check_words_across(WordList, Crossword, GridSize, {0, 0}, [], []).
+
+check_words_across(WordList, Crossword, GridSize, {X, Y} = Pos, Letters, Words) ->
+    {NewLetters, NewWords} = case maps:is_key(Pos, Crossword) of
+        true ->
+            {[maps:get(Pos, Crossword) | Letters], Words};
+        false ->
+            case length(Letters) of
+                0 ->
+                    {[], Words};
+                1 ->
+                    {[], Words};
+                _ ->
+                    {[], [lists:reverse(Letters) | Words]}
+            end
+    end,
+    NewY = Y + 1,
+    case NewY =:= GridSize of
+        true ->
+            AllWords = case length(Letters) of
+                0 -> NewWords;
+                _ -> [lists:reverse(NewLetters) | NewWords]
+            end,
+            ?debugFmt("Words for row ~p: ~p", [X, AllWords]),
+            NewX = X + 1,
+            case NewX =:= GridSize of
+                true ->
+                    validate_words(WordList, AllWords);
+                false ->
+                    check_words_across(WordList, Crossword, GridSize, {NewX, 0}, [], AllWords)
+            end;
+        false ->
+            check_words_across(WordList, Crossword, GridSize, {X, NewY}, NewLetters, NewWords)
+    end.
+
+check_words_down(WordList, Crossword, GridSize) ->
+    check_words_down(WordList, Crossword, GridSize, {0, 0}, [], []).
+
+check_words_down(WordList, Crossword, GridSize, {X, Y} = Pos, Letters, Words) ->
+    {NewLetters, NewWords} = case maps:is_key(Pos, Crossword) of
+        true ->
+            {[maps:get(Pos, Crossword) | Letters], Words};
+        false ->
+            case length(Letters) of
+                0 ->
+                    {[], Words};
+                1 ->
+                    {[], Words};
+                _ ->
+                    {[], [lists:reverse(Letters) | Words]}
+            end
+    end,
+    NewX = X + 1,
+    case NewX =:= GridSize of
+        true ->
+            AllWords = case length(NewLetters) of
+                0 -> NewWords;
+                _ -> [lists:reverse(NewLetters) | NewWords]
+            end,
+            ?debugFmt("Words for col ~p: ~p", [Y, AllWords]),
+            NewY = Y + 1,
+            case NewY =:= GridSize of
+                true ->
+                    validate_words(WordList, AllWords);
+                false ->
+                    check_words_down(WordList, Crossword, GridSize, {0, NewY}, [], AllWords)
+            end;
+        false ->
+            check_words_down(WordList, Crossword, GridSize, {NewX, Y}, NewLetters, NewWords)
+    end.
+
+validate_words(WordList, Words) ->
+    lists:all(fun(W) -> lists:member(W, WordList) end, Words).
